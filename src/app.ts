d5318393b901803e711d820fc056cd88bfe1c911
Azi-png <azizagoibnazarova24@gmail.com>
2025-03-15 20:48:09 +0900

    feat: express integrations

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express from "express";
+
+import path from "path";
+
+// 1. Entrance
+const app = express();
+console.log(__dirname);
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// 2. Sessions
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// 3.Views
+
+export default app;
--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,10 @@
 console.log("Hello world");
+/* H-TASK: 
+
+shunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul qilib, faqat positive qiymatlarni olib string holatda return qilsin
+MASALAN: getPositive([1, -4, 2]) return qiladi "12"*/
+function getPositive(a: number[]): string {
+  return a.filter((number) => number >= 0).join("");
+}
+
+console.log(getPositive([0, 5, 10, -8]));
