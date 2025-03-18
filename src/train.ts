9dad9189db318a271fe41d8d4451bfa1ff3791a4
Azi-png <azizagoibnazarova24@gmail.com>
2025-03-18 17:41:19 +0900

    feat: develop member schema model

--- src/train.ts
+++ src/train.ts
@@ -1,10 +1,20 @@
 console.log("Hello world");
+/*H2-TASK: 
+
+Shunday function tuzing, unga string argument pass bolsin. Function ushbu agrumentdagi digitlarni yangi stringda return qilsin
+MASALAN: getDigits("m14i1t") return qiladi "141"*/
+
+function getDigits(string: string): string {
+  return string.split("").filter(Number).join("");
+}
+console.log(getDigits("m14i1t"));
+
 /* H-TASK: 
 
 shunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul qilib, faqat positive qiymatlarni olib string holatda return qilsin
 MASALAN: getPositive([1, -4, 2]) return qiladi "12"*/
-function getPositive(a: number[]): string {
-  return a.filter((number) => number >= 0).join("");
-}
+// function getPositive(a: number[]): string {
+//   return a.filter((number) => number >= 0).join("");
+// }
 
-console.log(getPositive([0, 5, 10, -8]));
+// console.log(getPositive([0, 5, 10, -8]));
