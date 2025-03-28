72794872122f1620d81fe59b28c642147ff76b82
Azi-png <azizagoibnazarova24@gmail.com>
2025-03-28 11:44:15 +0900

    feat: integrate middleware sessions

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,42 @@
 import express from "express";
-
 import path from "path";
 import router from "./router";
 import routerAdmin from "./router-admin";
 import morgan from "morgan";
 import { MORGAN_FORMAT } from "./libs/config";
+import session from "express-session";
+import MongoDB from "connect-mongodb-session";
+import ConnectMongoDB from "connect-mongodb-session";
 
+const MongoDBStore = ConnectMongoDB(session);
+const store = new MongoDBStore({
+  uri: String(process.env.MONGO_URL),
+  collection: "sessions",
+});
 // 1. Entrance
 const app = express();
 console.log(__dirname);
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.json());
 app.use(morgan(MORGAN_FORMAT));
 
 // 2. Sessions
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.use(
+  session({
+    secret: "This is a secret",
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
+    },
+    store: store,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
 // 3.Views
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 //4.ROUTERS
 app.use("/admin", routerAdmin); //BSSR: EJS
