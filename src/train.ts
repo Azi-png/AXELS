12cf7efa9b27c089bc6982c68287a748fc4b5085
Azi-png <azizagoibnazarova24@gmail.com>
2025-04-15 13:01:44 +0900

    feat: task Q

--- src/train.ts
+++ src/train.ts
@@ -1,4 +1,27 @@
-console.log("Hello world"); /** */ /** */
+console.log("Hello world"); /** */
+/**TASK Q:
+
+Shunday function yozing, u 2 ta parametrga ega bo'lib
+birinchisi object, ikkinchisi string bo'lsin.
+Agar qabul qilinayotgan ikkinchi string, objectning
+biror bir propertysiga mos kelsa, 'true', aks holda mos kelmasa 'false' qaytarsin.
+
+MASALAN: hasProperty({ name: "BMW", model: "M3" }, "model"); return true;
+Ushbu misolda, 'model' string, objectning propertysiga mos kelganligi uchun 'true' natijani qaytarmoqda
+
+MASALAN: hasProperty({ name: "BMW", model: "M3" }, "year"); return false;
+Ushbu misolda, ikkinchi argument sifatida berilayotgan 'year' objectning
+propertysida mavjud bo'lmaganligi uchun 'false' natijani qaytarmoqda.
+ */
+
+function hasProperty(obj: any, prop: string): boolean {
+  return prop in obj;
+}
+const car = { name: "BMW", model: "M3" };
+
+console.log(hasProperty(car, "model"));
+console.log(hasProperty(car, "year"));
+
 /**TASK P:
 
 Parametr sifatida yagona object qabul qiladigan function yozing.
@@ -6,10 +29,10 @@ Qabul qilingan objectni nested array sifatida convert qilib qaytarsin
 
 MASALAN: objectToArray( {a: 10, b: 20}) return [['a', 10], ['b', 20]] */
 
-function objectToArray(obj: { [key: string]: any }): [string, any][] {
-  return Object.keys(obj).map((key): [string, any] => [key, obj[key]]);
-}
-console.log(objectToArray({ a: 10, b: 20 }));
+// function objectToArray(obj: { [key: string]: any }): [string, any][] {
+//   return Object.keys(obj).map((key): [string, any] => [key, obj[key]]);
+// }
+// console.log(objectToArray({ a: 10, b: 20 }));
 
 /**TASK O:
 
